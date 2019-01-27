8cae96a70993c0b2eb0e8f95c439aa3aed5562f7
Samuel Bizien Filippi <36996277+Samuel-BF@users.noreply.github.com>
2019-01-27 21:29:25 +0100

    End of 'Adding a relation' section (new attribute in room page).

--- src/Modules/App/Room.php
+++ src/Modules/App/Room.php
@@ -5,6 +5,7 @@ namespace App\Modules\App;
 use Sintattica\Atk\Core\Node;
 use Sintattica\Atk\Attributes\Attribute;
 use Sintattica\Atk\Attributes\NumberAttribute;
+use Sintattica\Atk\Relations\OneToManyRelation;
 use Sintattica\Atk\Security\SecurityManager;
 
 class Room extends Node
@@ -17,7 +18,8 @@ class Room extends Node
         $this->add(new Attribute('id', Attribute::AF_AUTOKEY));
         $this->add(new Attribute('name', Attribute::AF_OBLIGATORY));
         $this->add(new NumberAttribute('capacity'));
-        
+        $this->add(new OneToManyRelation('conferences', Attribute::AF_HIDE_LIST, 'app.conference', 'room'));
+
         $this->setDescriptorTemplate('[name]');
     }
 }
